564221b3ae42cc02b2b007f36a8f6484542b4d41
Randy Herasme <flatironschool@Randys-Macbook.local>
2019-02-04 17:23:34 -0500

    show page style

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -3,30 +3,62 @@ import React from "react";
 class ResultsPage extends React.Component {
   displaySentence = () => {
     let compiled = [];
-    this.props.results.problems.forEach(numberSentence => {
-      console.log(this.props.results.id);
-      console.log(this.props.submittedAnswers);
-      console.log(numberSentence.id);
+    let log = [];
+    let filteredAnswers = this.props.submittedAnswers.filter(
+      answer => answer.game_id === this.props.results.id
+    );
 
-      this.props.submittedAnswers.forEach(data => {
-        if (
-          this.props.results.id === data.game_id &&
-          numberSentence.id === data.problem_id
-        ) {
-          compiled.push(
-            <div className="col-6 col-sm-4">
-              {numberSentence.number_sentence} {data.answer}
-            </div>
-          );
+    // console.log(this.props.results.problems);
+    // console.log(filteredAnswers);
+
+    filteredAnswers.forEach(answer => {
+      this.props.results.problems.forEach(numberSentence => {
+        if (numberSentence.id === answer.problem_id) {
+          log.push(numberSentence.id);
+
+          if (log[log.length - 2] !== numberSentence.id) {
+            let splitData = numberSentence.number_sentence.split(/[\s=]+/);
+            console.log(splitData);
+            if (
+              splitData[1] === "+" &&
+              parseInt(splitData[0]) + parseInt(splitData[2]) === answer.answer
+            ) {
+              compiled.push(
+                <div className="col-6 col-sm-4">
+                  {numberSentence.number_sentence} {answer.answer}{" "}
+                  <i className="correctAnswer">&#x2713;</i>
+                </div>
+              );
+            } else if (
+              splitData[1] === "-" &&
+              parseInt(splitData[0]) - parseInt(splitData[2]) === answer.answer
+            ) {
+              compiled.push(
+                <div className="col-6 col-sm-4">
+                  {numberSentence.number_sentence} {answer.answer}{" "}
+                  <i className="correctAnswer">&#x2713;</i>
+                </div>
+              );
+            } else {
+              compiled.push(
+                <div className="col-6 col-sm-4">
+                  {numberSentence.number_sentence} {answer.answer}{" "}
+                  <i className="incorrectAnswer">&#x2718;</i>
+                </div>
+              );
+            }
+          }
         }
       });
     });
+
     return compiled;
   };
+
   render() {
     return (
       <div>
-        <h4 className="correct">Correct: {this.props.results.score}</h4>
+        <h4>Correct: {this.props.results.score}</h4>
         <h4>Time Left: {this.props.results.time_remaining} seconds</h4>
         <br />
         <div className="row">{this.displaySentence()}</div>
