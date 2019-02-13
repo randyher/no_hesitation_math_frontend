73d63f24a217bb9bc105916b5d6bbd6c5f525aab
Randy Herasme <flatironschool@Randys-Macbook.local>
2019-02-13 11:14:01 -0500

    home vid

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
 import Loading from "./Loading";
+import YoutubeBackground from "react-youtube-background";
 
 class Home extends React.Component {
   render() {
-    return <div className="theHome" />;
+    return (
+      <YoutubeBackground
+        videoId={"8cR_1Qi-tP4"}
+        aspectRatio={"16:9"}
+        overlay={"rgba(255,255,255,.5)"}
+        className={"video-background"}
+      >
+        <div className="theHome" />
+      </YoutubeBackground>
+    );
   }
 }
 
